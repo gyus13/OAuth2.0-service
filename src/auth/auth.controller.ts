1ba916bece4333c9195e65a6476bf8d404f51aec
gyus13 <gyusss13@naver.com>
2022-09-25 19:39:31 +0900

    Feat [#1] google 소셜로그인(id_토큰 기반)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,21 @@
 import { Controller } from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+    @ApiResponse({
+        status: 1000,
+        description: '성공',
+        type: SignInResponse,
+    })
+    @Post('ios/google')
+    @ApiOperation({ summary: 'IOS 구글 로그인' })
+    @ApiHeader({
+        description: 'id-token',
+        name: 'id-token',
+        example: 'id-TOKEN',
+    })
+    async GoogleAuth(@Headers('id-token') idToken) {
+        return this.authService.verifyGoogle(idToken);
+    }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,76 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    async iosVerifyGoogle(token) {
+        const queryRunner = this.connection.createQueryRunner();
+        await queryRunner.connect();
+        await queryRunner.startTransaction();
+        try {
+            const ticket = await client.verifyIdToken({
+                idToken: token,
+                audience: secret.ios_google_client_id, // Specify the CLIENT_ID of the app that accesses the backend
+                // Or, if multiple clients access the backend:
+                //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+            });
+            let data = {};
+            const payload = ticket.getPayload();
+            const accountId = payload['sub'];
+
+            let user = await this.userRepository.findOne({
+                where: { accountId: accountId },
+            });
+            let accountPayload;
+
+            // 유저가 존재하지 않는 경우
+            if (user == undefined) {
+                user = await this.userRepository.save({
+                    accountId: accountId,
+                });
+                accountPayload = { sub: user.id };
+                data = {
+                    accountId: accountId,
+                    nickname: null,
+                    characterImageUrl: null,
+                    token: this.jwtService.sign(accountPayload),
+                };
+            } else {
+                const characterImageUrl = await getManager()
+                    .createQueryBuilder(Character, 'characters')
+                    .innerJoin(CharacterUser, 'CU', 'characters.id = CU.characterId')
+                    .select('characters.characterImageUrl')
+                    .where('CU.userId IN (:userId)', { userId: user.id })
+                    .getOne();
+
+                accountPayload = { sub: user.id };
+                if (characterImageUrl == undefined) {
+                    data = {
+                        accountId: accountId,
+                        nickname: user.nickname,
+                        characterImageUrl: null,
+                        token: this.jwtService.sign(accountPayload),
+                    };
+                } else {
+                    data = {
+                        accountId: accountId,
+                        nickname: user.nickname,
+                        characterImageUrl: characterImageUrl.characterImageUrl,
+                        token: this.jwtService.sign(accountPayload),
+                    };
+                }
+            }
+
+            const result = makeResponse(response.SUCCESS, data);
+
+            await queryRunner.commitTransaction();
+
+            return result;
+        } catch (error) {
+            // Rollback
+            await queryRunner.rollbackTransaction();
+            return response.ERROR;
+        } finally {
+            await queryRunner.release();
+        }
+    }
+}
